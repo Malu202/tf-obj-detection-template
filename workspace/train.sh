288355ea3d465301c1290c77d918c18ac54a7567
Malu202 <29000299+Malu202@users.noreply.github.com>
2020-05-30 18:19:33 +0200

    Update train.sh

--- workspace/train.sh
+++ workspace/train.sh
@@ -1,3 +1,6 @@
+#Modify this line:
+config_filename=INSERT_CONFIG_FILENAME.config
+
 tensorboard --logdir output/ &
-python model_main.py --alsologtostderr --model_dir=output/ --pipeline_config_path=training/INSERT_CONFIG_FILENAME.config
+python model_main.py --alsologtostderr --model_dir=output/ --pipeline_config_path=training/$config_filename
 

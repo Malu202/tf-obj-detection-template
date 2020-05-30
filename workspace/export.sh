682db69c3c9654d54f17b7de50e2067def0dd8f8
Malu202 <29000299+Malu202@users.noreply.github.com>
2020-05-31 00:57:09 +0200

    Update export.sh

--- workspace/export.sh
+++ workspace/export.sh
@@ -1,6 +1,12 @@
+#Change these parameters:
+input_shape_width=300
+input_shape_height=300
+config_filename=model.config
+checkpoint_number=0000
+
 python scripts/export_inference_graph.py \
 	--input_type image_tensor \
-	--input_shape 300,300,3 \
-	--pipeline_config_path training/INSER_CONFIG_FILENAME.config \
-	--trained_checkpoint_prefix output/model.ckpt-INSERT_CKPT_NUMBER \
+	--input_shape 1,$input_shape_width,$input_shape_height,3 \
+	--pipeline_config_path training/$config_filename \
+	--trained_checkpoint_prefix output/model.ckpt-$checkpoint_number \
  	--output_directory export/
